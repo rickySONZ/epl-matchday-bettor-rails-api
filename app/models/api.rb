e8f2f405b4cbab32b6635c28837683e4ca0064b5
Ryan Erricson <ryanerricson@gmail.com>
2021-07-15 14:09:41 -0400

    Switched external pull to ESPN API, football-data did not have detailed match info

--- app/models/api.rb
+++ app/models/api.rb
@@ -3,9 +3,14 @@ require 'httparty'
 class Api < ApplicationRecord
 
     def self.pull_matchday_data()
-        @url = 'http://api.football-data.org/v2/competitions/PL/matches/?matchday=22'
-        match_array = HTTParty.get(@url, :headers => { 'X-Auth-Token' => ENV['SECRET'] } )
+        @url = 'https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/scoreboard?dates=20210812-20210819'
+        match_array = HTTParty.get(@url )
         # match_array.each do |match_hash|
         match_array
+    end
+
+    def pull_team_data()
+        url = 'http://api.football-data.org/v2/competitions/PL/matches/?matchday=22'
+        team_array = HTTParty.get(@url )
     end
 end
