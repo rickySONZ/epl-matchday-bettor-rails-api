f685997ad2593673240c2e7cd0eb18649fedd791
Ryan Erricson <ryanerricson@gmail.com>
2021-07-19 14:13:36 -0400

    created class method that will change dates and scoreboard dates for data pulls and scoreboard date setting

--- app/models/api.rb
+++ app/models/api.rb
@@ -3,6 +3,31 @@ require 'httparty'
 
 class Api < ApplicationRecord
 
+    @@end_date = '20210701'
+    @@start_date ='20210624'
+
+    def initialize
+        @@end_date = '20210701'
+        @@start_date ='20210624'
+    end
+
+
+    def self.change_dates
+        while Date.today > @@end_date.to_date do
+            @@start_date = @@end_date
+            @@end_date = @@end_date.to_date
+            @@end_date = (@@end_date + 7).to_s.gsub("-","")
+        end
+    end
+
+    def self.get_end_date
+        @@end_date
+    end
+
+    def self.get_start_date
+        @@start_date
+    end
+
     def self.pull_matchday_data()
         @url = 'https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/scoreboard?dates=20210812-20210819'
         match_array = HTTParty.get(@url)
