4c4b7c75ccb3595daf48edcf0541a51a73329c1d
Ryan Erricson <ryanerricson@gmail.com>
2021-07-27 12:17:43 -0400

    Scoreboard validations

--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -2,5 +2,6 @@ class Scoreboard < ApplicationRecord
     has_many :matches
     has_many :scores, through: :matches
 
-    
+    validates: :start_date, presence: true
+    validates: :end_date, presence: true
 end
